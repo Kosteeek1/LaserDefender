7d060f987d7339e64f8a93d045dca83ccefb4def
Kostek <p.kostecki22@gmail.com>
2023-05-31 01:46:42 +0200

    simple AI implementation

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http;
 using Mono.Cecil;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] private GameObject projectilePrefab;
+    [Header("General")] [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float projectileSpeed = 10f;
     [SerializeField] private float projectileLifetime = 5f;
-    [SerializeField] private float fireRate = 0.2f;
+    [SerializeField] private float baseFireRate = 0.2f;
+
+    [Header("AI")] [SerializeField] private float fireRateVariant = 0f;
+    [SerializeField] private float minimumFireRate = 0.1f;
     [SerializeField] private bool useAI;
 
-    public bool isFiring;
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
+
+    [HideInInspector] public bool isFiring;
 
     private Coroutine firingCoroutine;
+
+    void Awake()
+    {
+        InitBounds();
+    }
 
     void Start()
     {
@@ -52,7 +66,11 @@ public class Shooter : MonoBehaviour
             Rigidbody2D projectileRigidbody = projectileInstance.GetComponent<Rigidbody2D>();
             if (projectileRigidbody != null)
             {
-                if (useAI)
+                if (useAI && !IsOnScreen(projectileInstance))
+                {
+                    Destroy(projectileInstance);
+                }
+                else if (useAI && IsOnScreen(projectileInstance))
                 {
                     projectileRigidbody.velocity = -transform.up * projectileSpeed;
                 }
@@ -63,7 +81,30 @@ public class Shooter : MonoBehaviour
             }
 
             Destroy(projectileInstance, projectileLifetime);
-            yield return new WaitForSeconds(fireRate);
+            float fireDelay = Random.Range(baseFireRate - fireRateVariant, fireRateVariant + baseFireRate);
+            float timeToNextProjectile = Mathf.Clamp(fireDelay, minimumFireRate, float.MaxValue);
+
+            yield return new WaitForSeconds(timeToNextProjectile);
         }
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    bool IsOnScreen(GameObject projectile)
+    {
+        Rigidbody2D body = projectile.GetComponent<Rigidbody2D>();
+        if (body.position.x < maxBounds.x && body.position.x > minBounds.x && body.position.y < maxBounds.y &&
+            body.position.y > minBounds.y)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
